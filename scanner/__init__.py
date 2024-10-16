7e98a816c7d19d24a97ca02e78ae305faaf11759
Harsh <63919951+harsh11x@users.noreply.github.com>
2024-10-17 03:02:34 +0530

    Create __init__.py

---
+++ scanner/__init__.py
@@ -0,0 +1,8 @@
+from .sql_injection import detect_sql_injection
+from .xss import detect_xss
+from .csrf import detect_csrf  # Make sure this exists
+from .directory_traversal import detect_directory_traversal
+from .rce import detect_rce
+from .subdomain_takeover import detect_subdomain_takeover
+from .sql_injection import detect_sql_injection
+from .subdomain_takeover import detect_subdomain_takeover
